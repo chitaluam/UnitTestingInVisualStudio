244ee3b45d41be46b27056b8ad97adac0dcb6ef5
chitaluam <chitalu.am@gmail.com>
2022-02-06 16:58:58 -0800

    Third commit - SI business code complete

--- SimpleInterestCalculator.cs
+++ SimpleInterestCalculator.cs
@@ -8,5 +8,33 @@ namespace UnitTestingInVisualStudio
 {
     public class SimpleInterestCalculator
     {
+        /// <summary>
+        /// This class calculates the simple interest amount to be paid 
+        /// given:-
+        /// i = simeple interest
+        /// p = principal 
+        /// r = interest rate per year
+        /// t = time in years
+        /// a = amount
+        /// </summary>
+
+        public decimal SimpleInterestValue(decimal p, decimal r, int t)
+        {
+            decimal simpleInterestValueResult = 0.00m;
+
+            simpleInterestValueResult = p * (r / 100) * t;
+
+            return simpleInterestValueResult; 
+        }
+
+        public decimal SimpleInterestAmountValue(decimal p)
+        {
+            decimal simpleInterestAmountResult = 0.00m;
+
+            simpleInterestAmountResult = p + SimpleInterestValue(1500, 12, 2);
+
+            return simpleInterestAmountResult;
+        }
+
     }
 }
